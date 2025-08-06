16cfa6190d63057f5d9da8ab99b508639822e9a8
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:43:34 +0530

    Create length of string.py

---
+++ length of string.py
@@ -0,0 +1,14 @@
+
+
+str1="rakesh"
+str2="dolui"
+str3=len(str1)
+str4=len(str2)
+final_str=str1+" "+str2
+
+print(len(str1))
+print(len(str2))
+print(str3)
+print(str4)
+print(final_str)
+print(len(final_str))
