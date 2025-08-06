1cd2f710fc386d558e321eae77d662f6ab4e5bbe
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:38:12 +0530

    it's about arithmetic operator.

---
+++ arithmetic operator.py
@@ -0,0 +1,15 @@
+#arithmetic operator
+a=1000
+b=500
+print(a+b)
+print(a-b)
+print(a*b)
+
+# division operator always gives float value
+print(a/b)
+c=12
+d=5
+print(c/d)
+
+print(a%b)
+print(a**b)# it means a^b
