3820c92a0d9d5163e243658e04311ed2577f9f92
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:36:39 +0530

    it's about variable.

---
+++ variable.py
@@ -0,0 +1,19 @@
+
+name="Rakesh"
+age=20
+price=26.99
+
+print(name)
+print("name")# print as it is.
+print(age)
+print(price)
+
+print("my name is:",name)
+print("my age is:",age)
+
+age2=age
+print("my age is:",age2)
+
+
+
+
