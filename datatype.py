76b45624572bdab01faca391be7e0c12a087f681
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:37:16 +0530

    it's about datatype in python.

---
+++ datatype.py
@@ -0,0 +1,42 @@
+#string datatype
+name='Rakesh'
+name1="Rakesh"
+name2="""Rakesh"""
+
+#int datatype
+age=20
+age1=-20
+age2=0
+
+# float datatype
+price=26.99
+price1=-25.99
+
+# boolean datatype
+old=True
+old1=False
+
+# None datatype
+a=None
+
+print(type(name))
+print(name)
+print(name1)
+print(name2)
+
+print(type(age))
+print(age)
+print(age1)
+print(age2)
+
+print(type(price))
+print(price)
+print(price1)
+
+print(type(old))
+print(old)
+print(old1)
+
+print(type(a))
+print(a)
+
