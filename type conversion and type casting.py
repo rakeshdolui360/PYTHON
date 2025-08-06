851c81cdd84a48831b7f80975799e995a65afab7
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:40:26 +0530

    it's about type conversion and type casting.

---
+++ type conversion and type casting.py
@@ -0,0 +1,32 @@
+# int to float type conversion
+a=2
+b=4.25
+sum=(a+b)
+print(sum)
+
+# #additon between str and float not possible .so we have to convert either str to int or str to float.
+# c= "2.25"
+# d=6.25
+# sum=(c+d)
+# print(sum)
+# print(type(c))
+
+#string to int type casting
+c= int("2")
+d=6.25
+sum=(c+d)
+print(sum)
+print(type(c))
+
+#string to float type casting
+c= float("2")
+d=6.25
+sum=(c+d)
+print(sum)
+print(type(c))
+
+#float to string type casting
+e=3.14
+e=str(e)
+print(type(e))
+
