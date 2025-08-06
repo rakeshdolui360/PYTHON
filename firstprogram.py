8f99c01cee61e4301a1e746df2ea700ecb47e751
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:35:48 +0530

    Create firstprogram.py

---
+++ firstprogram.py
@@ -0,0 +1,14 @@
+
+print("hello world")
+print("Rakesh")
+
+# print in different line
+print("Rakesh is my name.")
+print("my age is 20.")
+# print in a single line.
+print("Rakesh is my name.","my age is 20.")
+
+# to print number no "" is required
+print(25)
+print(35)
+print(25+35)
