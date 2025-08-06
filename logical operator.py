d71c624a02387d6ea2b933c79b92d352d2c5d45e
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-06 11:39:38 +0530

    it's about logical operator.

---
+++ logical operator.py
@@ -0,0 +1,22 @@
+#not operator
+print(not True)
+print(not False)
+a=50
+b=30
+print(not (a>b))
+
+#and operator
+val1=True
+val2=True
+print(val1 and val2)
+c=50
+d=30
+print((c>d) and (c==d))
+
+#or operator
+num1=False
+num2=False
+print(num1 or num2)
+c=50
+d=30
+print((c>d) or (c==d))
