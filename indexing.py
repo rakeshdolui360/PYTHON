992ac0af625789e592a02b66ae9784df3de9f4fc
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-07 08:21:32 +0530

    Update indexing.py

--- indexing.py
+++ indexing.py
@@ -5,4 +5,7 @@ print(ch)
 print(str1[8])
 #it can also print space of string as print(str1[7]) 
 
+# str1[4]="@"
+# print(str1)
+# it gives error because indexing can not insert character in main str
 
