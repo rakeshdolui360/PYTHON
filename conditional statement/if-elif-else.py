bab4bde80fc4ac7c7aa20a82d1d59cbdc79a3ffb
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-08 11:50:09 +0530

    Create if-elif-else.py

---
+++ conditional statement/if-elif-else.py
@@ -0,0 +1,10 @@
+#signal color 
+color=input("enter signal color.")
+if(color=="red"):
+    print(" stop in signal.")
+elif(color=="yellow"):
+    print(" please wait.")
+elif(color=="green"):
+    print(" you may go.")
+else:
+    print("enter valid signal color.")
