d716024268264ae2c9155aec656d7754cb084a4b
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-09 10:51:10 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,30 @@
+# format
+marks=[67.4,76.5,6.0,98.0,87.3,76.4]
+print(marks)
+
+#  like string concatenation
+marks1=[67,5,48,393,35]
+marks3=marks+marks1
+print(marks3)
+print(type(marks))
+
+# length of list
+print(len(marks))
+
+# indexing in list
+print(marks[0])
+print(marks[1])
+print(marks[4])
+
+
+# in list we can store element of different types
+student=["rakesh",20,"male"]
+
+# updating element
+student[1]=21  
+# in list we can access and change or update element
+print(student)
+# in string we can access element but can not update it
+# str="i am a coder"
+# str[4]=o
+# print(str)
