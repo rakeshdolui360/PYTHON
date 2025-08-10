039d8b10f2ef802827fd181753433fb84476f423
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-10 11:53:06 +0530

    Create list insert.py

---
+++ list method py/list insert.py
@@ -0,0 +1,6 @@
+list=[5,7,48,38,34]
+#list.insert(index,element)  inserting element at index not like update or append.
+list.insert(2,56)
+print(list.insert(2,56)) 
+#  it will  print none. it will insert another 56 at index 2.
+print(list)
