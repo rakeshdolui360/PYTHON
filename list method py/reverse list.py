7ae108f2b33ee2574e33277be886f84634b89ae1
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-10 11:52:48 +0530

    Create reverse list.py

---
+++ list method py/reverse list.py
@@ -0,0 +1,5 @@
+list=[5,7,48,38,34]
+#list reverse 
+list.reverse()
+# print(list.reverse())  it will print none but ,it will reverse the reversed list.so it will return the original list.
+print(list)
