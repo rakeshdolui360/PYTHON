5d1b5185f05f1fbb45db8e3b31368f3f0a761493
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-10 11:52:24 +0530

    Create list sorting.py

---
+++ list method py/list sorting.py
@@ -0,0 +1,12 @@
+list=[5,7,48,38,34]
+#list.sorting 
+list.sort()
+print(list.sort())
+print(list)
+
+# list sorting with string element.it occurs according to alphabet.
+list1=["banana","apple","mango","tea"]
+list1.sort()
+print(list1.sort())
+print(list1)
+
