de6bfb73fd66c0bf92dbf53d3fbdf11121fcf870
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-10 11:52:34 +0530

    Create reverse list sorting.py

---
+++ list method py/reverse list sorting.py
@@ -0,0 +1,12 @@
+list=[5,7,48,38,34]
+ #  list sorting in reverse order.
+list.sort(reverse=True)
+print(list.sort(reverse=True))
+print(list)
+
+list1=["banana","apple","mango","tea"]
+# list sorting in reverse with string element
+list1.sort(reverse=True)
+print(list1.sort(reverse=True))
+print(list1)
+
