e530ed4e8e1bc35bc4027223d639781c40d59b49
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-10 11:52:10 +0530

    Create list append.py

---
+++ list method py/list append.py
@@ -0,0 +1,7 @@
+
+# in list.append(element) only one element can be inserted in the list.
+list=[5,7,48,38,34]
+list.append(4)
+print(list.append(4))
+# it prints none, but append 4 at the end.
+print(list)
