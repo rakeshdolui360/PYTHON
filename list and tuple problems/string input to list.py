263b3892110b717ff411aa3f29921735346f635c
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-11 08:13:56 +0530

    Create string input to list.py

---
+++ list and tuple problems/string input to list.py
@@ -0,0 +1,24 @@
+
+m1=input("enter first movie name:  ")
+m2=input("enter second movie name:  ")
+m3=input("enter third movie name:  ")
+mov=[m1,m2,m3]
+print(mov)
+
+mov1=[]
+m11=input("enter first movie name:  ")
+m22=input("enter second movie name:  ")
+m33=input("enter third movie name:  ")
+mov1.append(m11)
+mov1.append(m22)
+mov1.append(m33)
+print(mov1)
+
+
+mov2=[]
+mov2.append(input("enter first movie name:  "))
+mov2.append(input("enter second movie name:  "))
+mov2.append(input("enter third movie name:  "))
+print(mov2)
+
+
