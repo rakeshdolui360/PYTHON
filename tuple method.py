1eb92b7a1729d7645ccc2e365aea80bbb0995f74
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-11 08:13:50 +0530

    Create tuple method.py

---
+++ tuple method.py
@@ -0,0 +1,12 @@
+
+tup=(2,1,5,7,7,3,2,1)
+print(tup.index(1))
+print(tup.index(2))
+print(tup.index(7))
+#tup.index(element ) returns index of first occurance of element
+
+print(tup.count(7))
+print(tup.count(2))
+print(tup.count(1))
+#tup.count(element ) returns  total occurance of element
+
