f4884f1fba50e3c85042d5c1ae0d06ff96c8c9e0
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-12 09:32:29 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,53 @@
+# format
+info={
+    "name":"rakesh dolui",
+    "age":20,
+    "ygpa":7.4,
+    "indian":True,
+    "subject":["dsa","co","analog"],
+    "skill":("c","html","css"),
+    
+   "rakesh dolui":"name",
+    20 : "age",
+    7.4 : "ygpa",
+    True: "indian",
+    ("c","html","css"):"skill"
+    
+}
+# dictionaries store data in key:value pair. in case of key all datatypes like string, int,float,boolean  can be used but list ,tuples can not  be used as they are mutable.
+# in case of value all datatypes like string, int,float,boolean and list ,tuples can be used.
+# after each key:value pair , is to be given.
+print(info)
+print(type(info))
+
+# print value 
+#print(info[key])
+print(info["name"])
+print(info[7.4])
+
+#updation and new key: value pair assigned
+info["name"]="RD"
+info["state"]="west bengal"
+print(info)
+
+#empty dictionary
+null_dict={}
+# new key: value pair assigned in empty dictionary
+null_dict["name"]="rakesh"
+print(null_dict)
+
+#nested dictionaries
+student={
+    "name":"rakesh dolui",
+    "subjects":{
+        "phy":97,
+        "math":95,
+        "chem":96
+    },
+    "age":20
+}
+print(student)
+#print value from nested dictionaries
+print(student["name"])
+print(student["subjects"])
+print(student["subjects"]["math"])
