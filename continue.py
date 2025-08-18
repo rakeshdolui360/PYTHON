0d3fbd555a9539b70a3692b74275d3d4dd954138
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-18 10:37:27 +0530

    Create continue.py

---
+++ continue.py
@@ -0,0 +1,23 @@
+
+# continue terminates execution of current iteration and continues execution of the loop with the next iteration.
+i=0
+while i<=10:
+    if(i==5):
+        i+=1
+        continue
+    print(i)
+    i+=1
+    
+    
+    
+    
+    
+# odd number print
+i=1
+while i<=100:
+    if(i%2==0):
+        i+=1
+        continue
+    print(i)
+    i+=1
+    
