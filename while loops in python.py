1fc5c73484d7b1849bf22c278b99987334411b36
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-18 10:37:20 +0530

    Create while loops in python.py

---
+++ while loops in python.py
@@ -0,0 +1,62 @@
+print(" hello rakesh 10 times") 
+i=1
+while i<=10:
+    print("hello rakesh")
+    i+=1
+
+
+print("1 to 100")
+i=1
+while i<=100:
+    print(i)
+    i+=1
+
+    
+print("100 to 1")
+i=100
+while i>=1:
+    print(i)
+    i-=1
+
+
+print("number table")
+n=int(input("enter number  "))
+i=1
+while i<=10:
+    print(n*i)
+    i+=1
+
+
+print("a list containing square of 1 to 10")
+num=[]
+i=1
+while i<=10:
+    num.append(i*i)
+    i+=1
+print(num)
+
+
+print("print the element of given tuple")
+num=(1,4, 9, 16, 25, 36, 49, 64, 81, 100)
+idx=0
+while idx<=9:
+    print(num[idx])
+    idx+=1
+
+
+    
+print(" find a element of a given tuple")
+n=int(input("enter the element  "))
+num=(1,4, 9, 16, 25, 36, 49, 64, 81, 100,36)
+idx=0
+while idx<=10:
+    if(num[idx]==n):
+        print("element is found at index no",idx)
+    else:
+        print("founding...")
+    idx+=1
+  
+
+        
+
+
