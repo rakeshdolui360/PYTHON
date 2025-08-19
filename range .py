90f6fb476784c33efec3c15a6aabede60b5058f4
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-19 11:05:28 +0530

    Create range .py

---
+++ range .py
@@ -0,0 +1,48 @@
+#range(stop)
+for i in range(5):
+    print(i)
+    
+
+#range(start,stop)
+for i in range(1,21):
+    print(i)
+    
+    
+#range(start,stop,step)
+#ODD NUMBER PRINT 
+for i in range(1,50,2):
+    print(i)
+
+
+#EVEN NUMBER PRINT 
+for i in range(2,51,2):
+    print(i)
+    
+    
+    
+    
+#1 to 100 print 
+for i in range(1,101):
+    print(i)
+    
+    
+#100 to 1 print
+#step can be negative also.
+for i in range(100,0,-1):
+    print(i)
+    
+    
+# multiple of n 
+n=int(input("enter number  "))
+for i in range(1,11):
+    print(n*i)
+    
+    
+    
+    
+#pass statement in range 
+for i in range(10):
+    pass
+
+
+print("next work will continue by passing previous for loop.")
