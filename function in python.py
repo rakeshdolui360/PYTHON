a6f328509b63816b588c0cf45190e4a8aedba828
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-20 10:06:02 +0530

    Create function in python.py

---
+++ function in python.py
@@ -0,0 +1,29 @@
+#summation 
+def cal(a,b):
+    sum=a+b
+    print(sum)
+    return sum
+
+
+cal(2,3)
+cal(3,7)
+
+#average
+def cal_avg(a,b,c):
+    avg=(a+b+c)/3
+    print(avg)
+    return avg
+
+cal_avg(6,8,2)
+
+#none return
+def print_hello():
+    print("hello")
+    
+    
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+
