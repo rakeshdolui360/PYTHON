12ade0126bee9ea31717c4b703fc50d68c8c554f
rakeshdolui360 <rakeshdolui52@gmail.com>
2025-08-23 12:02:55 +0530

    Update factorial.py

--- recursion  in python/factorial.py
+++ recursion  in python/factorial.py
@@ -2,8 +2,9 @@
 def  fact(n):
     if(n==0 or n==1):
         return 1
-    else:
-        return n*fact(n-1)
+    return n*fact(n-1)
+    
+    # return is must .otherwise it will give error.
     
 
 print(fact(5))
